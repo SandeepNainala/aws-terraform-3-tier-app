edbbf3dc7d6884aecb124b6013fcd3bc9d9c1a65
sandeepnaidu <nainala_sandeep@yahoo.com>
2023-07-08 12:48:49 +0530

    3 tire application

---
+++ rds.tf
@@ -0,0 +1,13 @@
+module "rds" {
+  source            = "./modules/rds"
+  subnets           =  aws_subnet.private_subnets
+  vpc_id            = aws_vpc.main.id
+  from_sgs          = [aws_security_group.application_tire]
+  allocated_storage = var.allocated_storage
+  engine_version    = var.engine_version
+  multi_azs         = false
+  db_name           = var.db_name
+  db_username       = var.rds_db_username
+  db_password       = var.rds_db_password
+
+}
--- vars.tf
+++ vars.tf
@@ -1,25 +1,48 @@
 variable "region" {
-  type = string
+  type    = string
   default = "us-east-1"
 }
 
 variable "az" {
-  type = list(any)
+  type        = list(any)
   description = "availability zone of the public and private subnets"
-  default = ["us-east-1a","us-east-1b"]
+  default     = ["us-east-1a","us-east-1b"]
 }
 
 variable "main_cidr_block" {
-  type = string
+  type    = string
   default = "10.0.0.0/16"
 }
 
 variable "public_cidr_blocks" {
-  type = list(string)
+  type    = list(string)
   default = ["10.0.1.0/24","10.0.2.0/24"]
 }
 
 variable "private_cidr_blocks" {
-  type = list(string)
+  type    = list(string)
   default = ["10.0.3.0/24","10.0.4.0/24"]
+}
+
+variable "allocated_storage" {
+  type    = number
+  default = 10
+}
+variable "engine_version" {
+  type    = number
+  default = "5.7.37"
+}
+variable "db_name" {
+  type    = string
+  default = "mydb"
+}
+
+variable "rds_db_username" {
+  type    = string
+  default = "admin"
+}
+
+variable "rds_db_password" {
+  type    = string
+  default = "itismydb"
 }
