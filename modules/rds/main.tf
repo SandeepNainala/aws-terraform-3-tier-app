6349350eddfc080af4c8e7b11b625687d9abd916
sandeepnaidu <nainala_sandeep@yahoo.com>
2023-07-08 16:04:57 +0530

    3 tire application

--- modules/rds/main.tf
+++ modules/rds/main.tf
@@ -1,5 +1,5 @@
 resource "aws_db_instance" "rds" {
-  db_subnet_group_name        = aws_db_subnet_group.db-subnet-group.id
+  db_subnet_group_name        = aws_db_subnet_group.db_subnet_group.id
   allocated_storage           = var.allocated_storage
   engine                      = var.engine
   engine_version              = var.engine_version
@@ -10,4 +10,12 @@ resource "aws_db_instance" "rds" {
   skip_final_snapshot         = true
   vpc_security_group_ids      = [aws_security_group.]
 
+}
+resource "aws_db_subnet_group" "db_subnet_group" {
+  name       = "db_subnet_group"
+  subnet_ids = var.subnets.*.id
+
+  tags = {
+    Name = "db_subnet_group"
+  }
 }
