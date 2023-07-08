f4c0e70aa86e38aa60f687f4cc046ea07ac8e073
sandeepnaidu <nainala_sandeep@yahoo.com>
2023-07-08 12:25:11 +0530

    3 tire application

--- eip.tf
+++ eip.tf
@@ -1,6 +1,6 @@
 resource "aws_eip" "nat_ip" {
   count     = length(aws_subnet.public_subnets)
-  depends_on = [aws_internet_gateway.igw]
+  depends_on = [aws_internet_gateway.gw]
   tags{
     Name  = "nat_ip_${count.index+1}"
   }
