150515dd0cd5077a7927ec8a86807f3a8648104d
sandeepnaidu <nainala_sandeep@yahoo.com>
2023-07-09 10:34:34 +0530

    3 tire application

--- vars.tf
+++ vars.tf
@@ -2,6 +2,12 @@ variable "region" {
   type    = string
   default = "us-east-1"
 }
+variable "access_key" {
+  type = string
+}
+variable "secret_key" {
+  type = string
+}
 
 variable "az" {
   type        = list(any)
