587c1cae7db89714abdf7ea848ff8cf0d78068ac
sandeepnaidu <nainala_sandeep@yahoo.com>
2023-07-10 16:16:23 +0530

    3 tire application

--- ec2.tf
+++ ec2.tf
@@ -26,3 +26,36 @@ resource "aws_launch_template" "presentation_tire" {
   aws_lb.application_tire
   ]
 }
+
+resource "aws_launch_template" "application_tire" {
+  name = "application_tire"
+
+  block_device_mappings {
+    device_name = "/dev/xvda"
+
+    ebs {
+      volume_size = 8
+    }
+  }
+
+  key_name      = "key-aws-new-key.pem"
+  instance_type = "t2.micro"
+  image_id      = ""
+
+  network_interfaces {
+    associate_public_ip_address = false
+    security_groups             = [aws_security_group.application_tire.id]
+  }
+  user_data = base64encode(templatefile("app.sh", {
+    rds_host                  = module.rds.rds_address
+    rds_username              = var.rds_db_username
+    rds_password              = var.rds_db_password
+    rds_port                  = 3306,
+    rds_db_name               = var.db_name
+    region                    = "us-east-1"
+  }
+  ))
+  depends_on = [
+    aws_nat_gateway.gw
+  ]
+}
